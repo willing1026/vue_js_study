dcf772d78b420593bcb8e90304493ac20ee8f03e
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-11-04 17:37:43 +0900

    add : form기능 추가, axios 기능 추가

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,20 +1,47 @@
 <template>
-  <form action="">
+  <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
-      <input id="username" type="text">
+      <input id="username" type="text" v-model="username">
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="password">
+      <input id="password" type="password" v-model="password">
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  //컴포넌트간 값의 참조, 공유 발생하지 않게. 새 객체 생성
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function() {
+      console.log(this.username, this.password);
+
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+
+      axios.post(url, data)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    }
+  }
 }
 </script>
 
