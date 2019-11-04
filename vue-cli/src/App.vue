a30fe4e9131bd0c7095041410dc085077551c3a5
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-11-04 16:15:18 +0900

    add / delete : 불필요한 파일제거, 예제 실행

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 이 템플릿은 root.  하나의 엘리먼트만 존재 가능 -->
+  <!-- 여러개의 div 태그를 병렬로 선언 X -->
+  <div>
+    {{ str }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  // function으로 만들고, 새로운 객체 반환하도록 해야함
+  data: function(){
+      return {
+        str: 'hi'
+      }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
