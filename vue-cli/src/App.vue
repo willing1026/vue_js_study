7a1a1ed355949f215141781a9735fb5113045b4d
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-11-04 16:28:41 +0900

    add : appHeader Component 추가

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -2,18 +2,24 @@
   <!-- 이 템플릿은 root.  하나의 엘리먼트만 존재 가능 -->
   <!-- 여러개의 div 태그를 병렬로 선언 X -->
   <div>
-    {{ str }}
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
   // function으로 만들고, 새로운 객체 반환하도록 해야함
   data: function(){
       return {
         str: 'hi'
       }
+  },
+  components: {
+    'app-header': AppHeader
   }
+
 }
 </script>
 
